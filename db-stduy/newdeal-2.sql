a74f56b0b08acd1fd5f3f8e0991803aebfb4db0a
codingbang <codingbang@gmail.com>
2018-12-18 10:14:46 +0900

    file : add database.Ex09_Oracle_CallableStatement.java, dml.java, db-study.newdeal-2.sql

---
+++ db-stduy/newdeal-2.sql
@@ -0,0 +1,40 @@
+﻿create or replace procedure usp_emplist
+(
+   p_sal IN number,
+   p_cursor OUT SYS_REFCURSOR -- app ???? ???? ?????? ?????(Multi row)
+)
+is
+  BEGIN
+        OPEN p_cursor
+        FOR
+          select empno ,ename , sal from emp where sal > p_sal;
+
+END;
+
+create table dmlemp
+as
+ select empno, ename, job
+ from emp
+ where 1=2;
+
+
+create or replace procedure usp_insert_emp
+(
+   vempno IN emp.empno%TYPE,
+   vename IN emp.ename%TYPE,
+   vjob    IN emp.job%TYPE,
+   p_outmsg OUT varchar2
+)
+IS
+  BEGIN
+      insert into dmlemp(empno, ename, job)
+      values(vempno,vename,vjob);
+      COMMIT;
+        p_outmsg := 'SUCCESS';
+        EXCEPTION WHEN OTHERS THEN
+        p_outmsg := SQLERRM;
+        ROLLBACK;
+END; 
+
+alter table dmlemp
+add constraint pk_dmlemp_empno primary key(empno);
